5ca891e5fac716736f5b4e9a7cc125716acb0a4e
Leo Zhong <yunji@uber.com>
2020-04-04 22:35:24 -0700

    add user

--- src/main/java/com/example/springbootauth/SpringbootauthApplication.java
+++ src/main/java/com/example/springbootauth/SpringbootauthApplication.java
@@ -2,9 +2,16 @@ package com.example.springbootauth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class SpringbootauthApplication {
+
+	@Bean
+	public BCryptPasswordEncoder bCryptPasswordEncoder(){
+		return new BCryptPasswordEncoder();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootauthApplication.class, args);
